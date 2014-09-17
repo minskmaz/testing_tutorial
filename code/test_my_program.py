2ef5bba4749d1b296428d4ef1182674f48a275b8
Daniel Mizyrycki <mzdaniel@glidelink.net>
2014-09-17 14:44:04 -0700

    Add functional test

--- code/test_my_program.py
+++ code/test_my_program.py
@@ -18,6 +18,11 @@ class AverageTest(unittest.TestCase):
     def test_empty_arguments(self):
         self.assertEqual(average([]), None)
 
+class AverageFunctionalTest(unittest.TestCase):
     def test_cli_empty_arguments(self):
         out = getoutput('{}/{}'.format(CODE_PATH, 'my_program.py'))
         self.assertEqual(out, 'None')
+
+    def test_parse_cli_arguments(self):
+        out = getoutput('{}/{} 1 2 3 4 5'.format(CODE_PATH, 'my_program.py'))
+        self.assertEqual(out, '3.0')
