4ced8969e0cf092aacd75a07f3bfbb00c4c30fab
Daniel Mizyrycki <mzdaniel@glidelink.net>
2014-09-17 02:18:32 -0700

    Add test_prj.sh for testing automation

---
+++ code/test_prj.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set test directory where this script is located
+cd $(dirname $0)
+
+echo "Run doctests"
+python -m doctest -v my_program.py
+
+echo
+echo "Run unitests"
+python -m unittest discover
